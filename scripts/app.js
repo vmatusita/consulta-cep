3bcbc37d2518b8e0626b481b2c62338a97634fba
Victor Yuki Matusita <147436014+vmatusita@users.noreply.github.com>
2024-05-28 19:33:30 -0300

    arrumando dialog

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,25 @@
+function exibirEnderecosSalvos() {
+    const tabela = document.getElementById('table');
+    let enderecosSalvos = JSON.parse(localStorage.getItem('enderecos')) || [];
+    enderecosSalvos.forEach(endereco => {
+        let tr = document.createElement('tr');
+        tr.classList.add('border-b-2', 'border-collapse', 'border-text');
+        ['logradouro', 'bairro', 'localidade', 'uf', 'cep'].forEach(chave => {
+            let td = document.createElement('td');
+            td.classList.add('px-5', 'py-1.5');
+            td.textContent = endereco[chave];
+            tr.appendChild(td);
+        });
+        tabela.appendChild(tr);
+    });
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     const cepInput = document.getElementById('input-cep');
     const submitButton = document.getElementById('submit');
     const guardarCep = document.getElementById('guardar');
-    const novaBusca = document.getElementById('novo');
     const dialog = document.querySelector('dialog');
     let backdrop = document.getElementById('backdrop');
-
-    dialog.close();
 
     cepInput.addEventListener('input', function (e) {
         let value = e.target.value;
@@ -31,6 +44,19 @@ document.addEventListener('DOMContentLoaded', function () {
                 document.getElementById('cep').textContent = data.cep;
                 dialog.showModal();
                 backdrop.style.display = 'block';
+                guardarCep.dataset.endereco = JSON.stringify(data);
             });
     });
+
+    guardarCep.addEventListener('click', function (e) {
+        e.preventDefault();
+        const endereco = JSON.parse(guardarCep.dataset.endereco);
+        let enderecosSalvos = JSON.parse(localStorage.getItem('enderecos')) || [];
+        enderecosSalvos.push(endereco);
+        localStorage.setItem('enderecos', JSON.stringify(enderecosSalvos));
+        alert('Endereço salvo com sucesso!');
+        dialog.close();
+    });
+
+    exibirEnderecosSalvos();
 });
